abc8b9490f788a96f467821866153dcd745389de
hokageCodes <hashsenju94@gmail.com>
2024-06-13 15:09:53 +0100

    Added stripe test integration

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const UserRoute = require("./routes/userRoute");
 const authRoute = require("./routes/authRoute")
 const productRoute = require("./routes/productRoute")
 const cartRoute = require("./routes/cartRoute")
-const orderRoute = require("./routes/orderRoute")
+const orderRoute = require("./routes/orderRoute");
+const stripeRoute = require("./routes/stripeRoute")
+const cors = require("cors");
+
 
 dotenv.config();
 
@@ -16,13 +19,16 @@ mongoose.connect(process.env.MONGO_URI).then(() => (
     console.log("Error Connecting to DB", err)
 })
 
+app.use(cors());
 app.use(express.json())
-// routes
+
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", UserRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
